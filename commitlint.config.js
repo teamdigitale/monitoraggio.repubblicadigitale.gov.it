aa5adb1f933f13e964bda2c321977db705102e91
Marco Lovazzano <87663863+marcolovazzano@users.noreply.github.com>
2022-05-16 19:13:10 +0200

    feat: add commitlint config

---
+++ commitlint.config.js
@@ -0,0 +1,6 @@
+// Use of the shareable commitlint config in order to enforcing conventional commits
+// https://commitlint.js.org/
+
+module.exports = {
+  extends: ['@commitlint/config-conventional']
+};
