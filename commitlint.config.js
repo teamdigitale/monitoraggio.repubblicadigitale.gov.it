9cb41f449fa0d994972155c1c9a99422bc54baca
Marco Lovazzano <87663863+marcolovazzano@users.noreply.github.com>
2022-06-10 06:58:57 +0200

    chore: manage dependabot commit message

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,4 +3,11 @@
 
 module.exports = {
   extends: ['@commitlint/config-conventional']
+  /*
+   * Any rules defined here will override rules from @commitlint/config-conventional
+   */
+  rules: {
+    // manage dependabot commit message
+    'header-max-length': [2, 'always', 200],
+  }
 };
