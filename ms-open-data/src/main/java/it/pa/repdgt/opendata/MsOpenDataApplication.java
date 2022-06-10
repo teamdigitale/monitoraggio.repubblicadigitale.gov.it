6e00a0a1196b40aed2ff4c2380f32e4b72fb6c7d
carloalbertosimoncini <c.simoncini@accenture.com>
2022-06-10 11:28:05 +0200

    feat: aggiunta modulo base ms-open-data

---
+++ ms-open-data/src/main/java/it/pa/repdgt/opendata/MsOpenDataApplication.java
@@ -0,0 +1,13 @@
+package it.pa.repdgt.opendata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsOpenDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsOpenDataApplication.class, args);
+	}
+
+}
