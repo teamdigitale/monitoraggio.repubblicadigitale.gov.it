d5d0a593a7df603b8fb88e44f5dd507424cc5333
carloalbertosimoncini <c.simoncini@accenture.com>
2022-06-21 13:15:05 +0200

    fix(be): refactor ms-ente

--- ms-ente/src/main/java/it/pa/repdgt/shared/repository/storico/StoricoEnteGestoreProgettoRepository.java
+++
@@ -1,9 +0,0 @@
-package it.pa.repdgt.shared.repository.storico;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import it.pa.repdgt.shared.entity.storico.StoricoEnteGestoreProgettoEntity;
-
-@Repository
-public interface StoricoEnteGestoreProgettoRepository extends JpaRepository<StoricoEnteGestoreProgettoEntity, Long> { }
--- ms-ente/src/main/java/it/pa/repdgt/shared/repository/storico/StoricoEnteGestoreProgrammaRepository.java
+++
@@ -1,9 +0,0 @@
-package it.pa.repdgt.shared.repository.storico;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import it.pa.repdgt.shared.entity.storico.StoricoEnteGestoreProgrammaEntity;
-
-@Repository
-public interface StoricoEnteGestoreProgrammaRepository extends JpaRepository<StoricoEnteGestoreProgrammaEntity, Long> { }
