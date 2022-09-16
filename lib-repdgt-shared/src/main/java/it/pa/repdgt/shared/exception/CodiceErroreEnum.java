5fa19e6101487e14c382417cc981225de04b0a58
carloalbertosimoncini <c.simoncini@accenture.com>
2022-09-16 13:03:33 +0200

    fix(be): fix per check utenza se presente a sistema

--- lib-repdgt-shared/src/main/java/it/pa/repdgt/shared/exception/CodiceErroreEnum.java
+++ lib-repdgt-shared/src/main/java/it/pa/repdgt/shared/exception/CodiceErroreEnum.java
@@ -53,8 +53,8 @@ public enum CodiceErroreEnum {
 	,U17("Errore download immagine profilo utente")
 	,U18("Errore Associazione tra utente e ruolo non trovata.")
 	,U19("Errore export csv utenti")
+	,U20("Errore utente con codice fiscale non esistente")
 
-	
 	,S01("Errore file upload cittadino non valido")
 	,S02("Errore export csv servizio")
 	,S03("Errore servizio non accessibile per l'utente")
