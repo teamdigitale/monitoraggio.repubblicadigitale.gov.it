95cb1850dbf741ae8a2b19cf7186531a497ea02a
niccolo.valente <niccolo.valente@amalflfk62fhn.fritz.box>
2022-10-11 13:48:44 +0200

    feat(fe-piattaforma): fe updates 10112022

--- fe-piattaforma/src/pages/common/OpenData/openData.tsx
+++ fe-piattaforma/src/pages/common/OpenData/openData.tsx
@@ -3,24 +3,31 @@ import { useDispatch } from 'react-redux';
 import clsx from 'clsx';
 import { Button, Container, Icon } from 'design-react-kit';
 import PageTitle from '../../../components/PageTitle/pageTitle';
-import { hideLoader, showLoader } from '../../../redux/features/app/appSlice';
+import {
+  hideLoader,
+  selectDevice,
+  showLoader,
+} from '../../../redux/features/app/appSlice';
 import API from '../../../utils/apiHelper';
 import { InfoPanel, Table } from '../../../components';
 import { newTable } from '../../../components/Table/table';
 import { staticValues, TableHeading } from './utils';
-import moment from 'moment';
 import {
   openDataBody,
   openDataSubtitle,
 } from '../../../components/SectionInfo/bodies';
 import { downloadFile } from '../../../utils/common';
+import { useAppSelector } from '../../../redux/hooks';
 
 const tableValues = newTable(TableHeading, staticValues);
 
 const OpenData = () => {
   const dispatch = useDispatch();
   const [totalCount, setTotalCount] = useState<string>('-');
+  const [dateCoverage, setDateCoverage] = useState<string>('-');
+  const [lastDate, setLastDate] = useState<string>('-');
   const [docSize, setDocSize] = useState<number>(0);
+  const device = useAppSelector(selectDevice);
 
   const getDocumentUrl = async () => {
     try {
@@ -39,6 +46,8 @@ const OpenData = () => {
       dispatch(showLoader());
       const resCount = await API.get('open-data/count/download');
       setTotalCount(resCount.data?.conteggioDownload?.toString());
+      setDateCoverage(resCount.data?.anniCopertura?.toString());
+      setLastDate(resCount.data?.dataUltimaPubblicazione?.toString());
       setDocSize(
         Math.max(1, Math.floor(Number(resCount.data?.dimensioneFile) / 1000))
       );
@@ -53,51 +62,61 @@ const OpenData = () => {
     getOpenData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+  useEffect(() => {
+    getOpenData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dateCoverage, lastDate]);
+
 
   return (
-    <Container>
-      <PageTitle title='Open Data' innerHTML HTMLsubtitle={openDataSubtitle} />
-      <InfoPanel openData HTMLlist body={openDataBody} colsNo={0} />
-      <div className='d-flex justify-content-end pt-4'>
-        <Button color='primary' onClick={getDocumentUrl}>
-          Scarica CSV {docSize ? `(${docSize} Kb)` : null}
-        </Button>
+    <Container className='py-5'>
+      <PageTitle
+        title='Open data: servizi di facilitazione e formazione per i cittadini'
+        innerHTML
+        HTMLsubtitle={openDataSubtitle}
+      />
+      <InfoPanel openData HTMLlist body={openDataBody(dateCoverage, lastDate)} colsNo={0} />
+      <div
+        className={clsx(
+          'd-flex',
+          device.mediaIsPhone ? 'flex-column' : 'flex-row',
+          'justify-content-between',
+          'align-items-center',
+          'pr-2',
+          'py-2',
+          'mt-5'
+        )}
+      >
+        <div className='d-flex'>
+          <span>Scarica i dati</span>
+        </div>
+        <div className='d-flex'>
+          <Button color='primary' onClick={getDocumentUrl}>
+            Scarica CSV {docSize ? `(${docSize} Kb)` : null}
+          </Button>
+        </div>
+        <div className={clsx('d-flex', 'flex-row', 'align-items-center')}>
+          <Icon icon='it-download' color='primary' size='' className='pr-2' />
+          {totalCount} Download
+        </div>
       </div>
       <div className={clsx('mt-5')}>
-        <Table {...tableValues} />
         <div
           className={clsx(
             'd-flex',
-            'flex-row',
-            'justify-content-end',
-            'pr-2',
-            'py-2'
+            'flex-column',
+            'lightgrey-bg-a1',
+            'align-items-center',
+            'text-center',
+            'my-3',
+            'py-4'
           )}
         >
-          <div
-            className={clsx(
-              'pr-4',
-              'd-flex',
-              'flex-row',
-              'justify-content-end',
-              'align-items-center'
-            )}
-          >
-            <Icon icon='it-calendar' color='primary' size='' className='pr-2' />
-            {moment().format('DD/MM/yyyy')}
-          </div>
-          <div
-            className={clsx(
-              'd-flex',
-              'flex-row',
-              ',justify-content-end',
-              'align-items-center'
-            )}
-          >
-            <Icon icon='it-download' color='primary' size='' className='pr-2' />
-            {totalCount}
-          </div>
+          <strong>Servizi di facilitazione e formazione</strong>
+          Modello di lettura del file di dati con elenco delle colonne e
+          relative descrizioni
         </div>
+        <Table {...tableValues} />
       </div>
     </Container>
   );
--- fe-piattaforma/src/pages/common/OpenData/utils.ts
+++ fe-piattaforma/src/pages/common/OpenData/utils.ts
@@ -7,9 +7,9 @@ export const TableHeading: TableHeadingI[] = [
     size: 'medium',
   },
   {
-    label: 'Tipo',
+    label: 'Tipo di dato',
     field: 'attributo_tipo',
-    size: 'small',
+    size: 'medium',
   },
   {
     label: 'Descrizione',
@@ -17,7 +17,7 @@ export const TableHeading: TableHeadingI[] = [
     size: 'medium',
   },
   {
-    label: 'Tipo',
+    label: 'Valori possibili',
     field: 'descrizione_tipo',
     size: 'medium',
   },
@@ -25,127 +25,141 @@ export const TableHeading: TableHeadingI[] = [
 
 export const staticValues = [
   {
+    attributo: 'ID',
+    attributo_tipo: 'Numerico',
+    descrizione:
+      'ID univoco della fruizione del servizio da parte di un singolo cittadino',
+    descrizione_tipo: '-',
+  },
+  {
+    attributo: 'ANNO_DI_NASCITA',
+    attributo_tipo: 'Numerico',
+    descrizione: "È l'anno di nascita del cittadino",
+    descrizione_tipo: '-',
+  },
+  {
     attributo: 'GENERE',
-    attributo_tipo: 'string',
+    attributo_tipo: 'Categorico',
     descrizione: 'È il genere del cittadino',
     descrizione_tipo: 'Donna, Uomo, Non binario',
   },
   {
+    attributo: 'OCCUPAZIONE',
+    attributo_tipo: 'Categorico',
+    descrizione: 'È lo stato occupazionale del cittadino ',
+    descrizione_tipo:
+      'Dipendente a tempo indeterminato, Dipendente a tempo determinato, Imprenditore, Libero professionista, Commerciante/artigiano, Inoccupato/a, Disoccupato/a, Volontario/a, Studente o studentessa / in formazione, Persona ritirata dal lavoro / Pensionato/a, Casalinga/o, Inabile al lavoro, Altro',
+  },
+  {
     attributo: 'TITOLO_DI_STUDIO',
-    attributo_tipo: 'string',
+    attributo_tipo: 'Categorico',
     descrizione:
       'È il titolo di studio massimo più alto raggiunto dal cittadino',
     descrizione_tipo:
       'Licenza elementare / Scuola primaria, Licenza media inferiore / Scuola secondaria di I grado (3 anni), Diploma di scuola superiore (diploma di maturità) / Scuola secondaria di II grado (5 anni), Diploma professionale (3 anni) / Istituti professionali / Istituti di Istruzione e Formazione Professionale (IeFP), Laurea (3 anni), Laurea magistrale (5 anni) / Master di I livello / Specializzazione post-laurea (2 anni), Dottorato, Master di II livello o titoli equiparati, Non conosciuto / non fornito / Altro',
   },
   {
-    attributo: 'ANNO_DI_NASCITA',
-    attributo_tipo: 'string',
-    descrizione: "È l'anno di nascita del cittadino",
-    descrizione_tipo: '-',
-  },
-  {
-    attributo: 'OCCUPAZIONE',
-    attributo_tipo: 'string',
-    descrizione: 'È lo stato occupazionale del cittadino ',
-    descrizione_tipo:
-      'Dipendente a tempo indeterminato, Dipendente a tempo determinato, Imprenditore, Libero professionista, Commerciante/artigiano, Inoccupato/a, Disoccupato/a, Volontario/a, Studente o studentessa / in formazione, Persona ritirata dal lavoro / Pensionato/a, Casalinga/o, Inabile al lavoro, Altro',
-  },
-  {
-    attributo: 'REGIONE',
-    attributo_tipo: 'string',
-    descrizione:
-      'È la regione dove viene erogato il servizio di facilitazione/formazione al cittadino ',
-    descrizione_tipo: '-',
-  },
-  {
-    attributo: 'PROVINCIA',
-    attributo_tipo: 'string',
-    descrizione:
-      'È la provincia dove viene erogato il servizio di facilitazione/formazione al cittadino ',
-    descrizione_tipo: '-',
-  },
-  {
-    attributo: 'COMUNE',
-    attributo_tipo: 'string',
-    descrizione:
-      'È il comune dove viene erogato il servizio di facilitazione/formazione al cittadino ',
-    descrizione_tipo: '-',
-  },
-  {
-    attributo: 'CAP',
-    attributo_tipo: 'string',
-    descrizione:
-      'È il codice di avviamento postale che identifica il comune dove viene erogato il servizio di facilitazione/formazione al cittadino  ',
-    descrizione_tipo: '-',
-  },
-  {
-    attributo: 'SEDE_ID',
-    attributo_tipo: 'string',
-    descrizione:
-      'È un codice interno che identifica univocamente la sede che eroga il servizio di...',
-    descrizione_tipo: '-',
-  },
-  {
-    attributo: 'NOME_SEDE',
-    attributo_tipo: 'string',
-    descrizione:
-      'È il nome della sede presso la quale viene erogato il servizio di ...',
-    descrizione_tipo: '-',
-  },
-  {
-    attributo: 'TIPOLOGIA-SERVIZIO',
-    attributo_tipo: 'string',
-    descrizione: 'È il tipo di servizio prenotato dal cittadino',
-    descrizione_tipo:
-      'Facilitazione, Formazione in presenza, Formazione online, Utilizzo postazione/ Dispositivi digitali, Altro',
-  },
-  {
     attributo: 'SERVIZIO_ID',
-    attributo_tipo: 'string',
+    attributo_tipo: 'Numerico',
     descrizione:
       'È un codice interno che identifica univocamente il servizio erogato',
     descrizione_tipo: '-',
   },
   {
     attributo: 'NOME_SERVIZIO',
-    attributo_tipo: 'string',
+    attributo_tipo: 'String',
     descrizione: 'È il nome del servizio erogato al cittadino ',
     descrizione_tipo: '-',
   },
   {
-    attributo: 'COMPETENZE_TRATTATE',
-    attributo_tipo: 'string',
+    attributo: 'DATA_FRUIZIONE_SERVIZIO',
+    attributo_tipo: 'Numerico',
     descrizione:
-      "Sono l'insieme delle competenze oggetto del servizio erogato al cittadino",
-    descrizione_tipo:
-      'Alfabetizzazione su informazioni e dati, Comunicazione e collaborazione, Creazione di contenuti digitali, Sicurezza, Risolvere i problemi tecnici',
+      'È la data in cui è stato erogato il servizio di facilitazione /formazione ',
+    descrizione_tipo: '-',
   },
   {
     attributo: 'AMBITO_SERVIZI_PUBBLICI_DIGITALI',
-    attributo_tipo: 'string',
+    attributo_tipo: 'Categorico',
     descrizione:
       "Sono l'insieme degli ambiti dei servizi pubblici digitali oggetto del servizio erogato al cittadino ",
     descrizione_tipo:
       "App IO, Sistemi di pagamenti elettronici (pagoPA), Servizi anagrafici tramite ANPR, Fascicolo sanitario elettronico, Fatturazione elettronica, Cultura e turismo, Istruzione, Formazione, Sport, Servizi di sostegno all'occupazione, Commercio e impresa, Servizi previdenziali e assistenziali, Servizi sanitari diversi da FSE Adempimenti fiscali, Servizi tributari e contravvenzioni, Urbanistica ed edilizia, Infrastrutture e mobilità , Utilizzo di piattaforme di partecipazione, Nessuna delle precedenti",
   },
   {
-    attributo: 'ID_PROGRAMMA',
-    attributo_tipo: 'string',
+    attributo: 'TIPOLOGIA-SERVIZIO',
+    attributo_tipo: 'Categorico',
+    descrizione: 'È il tipo di servizio prenotato dal cittadino',
+    descrizione_tipo:
+      'Facilitazione, Formazione in presenza, Formazione online, Utilizzo postazione/ Dispositivi digitali, Altro',
+  },
+  {
+    attributo: 'COMPETENZE_TRATTATE',
+    attributo_tipo: 'Categorico',
     descrizione:
-      'È un codice interno che identifica il programma per cui si eroga il servizio di facilitazione/ formazione',
+      "Sono l'insieme delle competenze oggetto del servizio erogato al cittadino",
+    descrizione_tipo:
+      'Alfabetizzazione su informazioni e dati, Comunicazione e collaborazione, Creazione di contenuti digitali, Sicurezza, Risolvere i problemi tecnici',
+  },
+  {
+    attributo: 'SEDE_ID',
+    attributo_tipo: 'Numerico',
+    descrizione:
+      'È un codice interno che identifica univocamente la sede che eroga il servizio di...',
+    descrizione_tipo: '-',
+  },
+  {
+    attributo: 'NOME_SEDE',
+    attributo_tipo: 'String',
+    descrizione:
+      'È il nome della sede presso la quale viene erogato il servizio di ...',
+    descrizione_tipo: '-',
+  },
+  {
+    attributo: 'REGIONE',
+    attributo_tipo: 'String',
+    descrizione:
+      'È la regione dove viene erogato il servizio di facilitazione/formazione al cittadino ',
+    descrizione_tipo: '-',
+  },
+  {
+    attributo: 'PROVINCIA',
+    attributo_tipo: 'String',
+    descrizione:
+      'È la provincia dove viene erogato il servizio di facilitazione/formazione al cittadino ',
+    descrizione_tipo: '-',
+  },
+  {
+    attributo: 'COMUNE',
+    attributo_tipo: 'String',
+    descrizione:
+      'È il comune dove viene erogato il servizio di facilitazione/formazione al cittadino ',
+    descrizione_tipo: '-',
+  },
+  {
+    attributo: 'CAP',
+    attributo_tipo: 'Numerico',
+    descrizione:
+      'È il codice di avviamento postale che identifica il comune dove viene erogato il servizio di facilitazione/formazione al cittadino  ',
     descrizione_tipo: '-',
   },
   {
     attributo: 'INTERVENTO',
-    attributo_tipo: 'string',
+    attributo_tipo: 'Categorico',
     descrizione:
       'È il tipo di intervento a cui sono associati i servizi di facilitazione/ formazione erogati',
     descrizione_tipo: 'RFD, SCD',
   },
   {
+    attributo: 'ID_PROGRAMMA',
+    attributo_tipo: 'Numerico',
+    descrizione:
+      'È un codice interno che identifica il programma per cui si eroga il servizio di facilitazione/ formazione',
+    descrizione_tipo: '-',
+  },
+  {
     attributo: 'ID_PROGETTO',
-    attributo_tipo: 'string',
+    attributo_tipo: 'Numerico',
     descrizione:
       'È un codice interno che identifica il progetto per cui si eroga il servizio di facilitazione/ formazione',
     descrizione_tipo: '-',
