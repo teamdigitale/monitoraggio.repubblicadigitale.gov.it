7514a19bd8d6dfc42d5df25cd3b826132fdae8ce
Niccolo Valente <niccolo.valente@accenture.com>
2022-10-27 17:57:22 +0200

    feat(fe-piattaforma): fe updates 10272022

--- fe-piattaforma/src/redux/features/forum/comments/commentsThunk.ts
+++ fe-piattaforma/src/redux/features/forum/comments/commentsThunk.ts
@@ -100,7 +100,7 @@ export const DeleteComment =
       dispatch(showLoader());
       dispatch({ ...DeleteCommentAction });
       await proxyCall(`/comment/${commentId}/delete`, 'POST', {
-        reason: reason.trim() !== "" ? reason : 'owner',
+        reason: reason?.trim() !== "" ? reason : 'owner',
       });
     } catch (error) {
       console.log('DeleteComment error', error);
