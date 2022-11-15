70d466b7aa7669c1bf2cf556c3592613a5bbfd8b
Niccolo Valente <36986000+nicogheart1@users.noreply.github.com>
2022-11-15 18:33:44 +0100

    feat(fe-piattaforma): fe updates 11152022

--- fe-piattaforma/src/App.tsx
+++ fe-piattaforma/src/App.tsx
@@ -14,6 +14,7 @@ import UsersAnagraphic from './components/UsersAnagraphic/usersAnagraphic';
 i18nInit();
 
 const MyApp: React.FC = () => {
+  console.log('11152022');
   return (
     <Provider store={store}>
       <FontLoader />
