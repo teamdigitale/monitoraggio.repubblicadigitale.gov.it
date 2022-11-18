a8db6abd5fe89dfaa6379a4406f85f1beeb34f00
carloalbertosimoncini <c.simoncini@accenture.com>
2022-11-18 09:06:35 +0100

    fix(be): fix regexp e separatore

--- ms-notifiche-integration/src/main/java/it/pa/repdgt/integrazione/restapi/DrupalRestApi.java
+++ ms-notifiche-integration/src/main/java/it/pa/repdgt/integrazione/restapi/DrupalRestApi.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.deser.impl.CreatorCandidate.Param;
 
 import it.pa.repdgt.integrazione.exception.DrupalException;
 import it.pa.repdgt.integrazione.request.ForwardRichiestDrupalParam;
