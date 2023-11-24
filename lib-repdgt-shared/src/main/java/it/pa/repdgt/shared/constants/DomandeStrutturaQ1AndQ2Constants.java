8a94596c24e06753604007bf1080f97332b46ded
Arturo Buono <a.buono@itdm.it>
2023-11-24 09:35:11 +0100

    feat(lib-repdgt-shared): updates 20231124

--- lib-repdgt-shared/src/main/java/it/pa/repdgt/shared/constants/DomandeStrutturaQ1AndQ2Constants.java
+++ lib-repdgt-shared/src/main/java/it/pa/repdgt/shared/constants/DomandeStrutturaQ1AndQ2Constants.java
@@ -8,7 +8,7 @@ public interface DomandeStrutturaQ1AndQ2Constants {
 	public final String ID_DOMANDA_TIPO_DOCUMENTO = "5";
 	public final String ID_DOMANDA_NUMERO_DOCUMENTO = "6";
 	public final String ID_DOMANDA_GENERE = "7";
-	public final String ID_DOMANDA_ANNO_DI_NASCITA = "8";
+	public final String ID_DOMANDA_FASCIA_DI_ETA = "8";
 	public final String ID_DOMANDA_TITOLO_DI_STUDIO = "9";
 	public final String ID_DOMANDA_STATO_OCCUPAZIONALE = "10";
 	public final String ID_DOMANDA_CITTADINANZA = "11";
@@ -20,7 +20,7 @@ public interface DomandeStrutturaQ1AndQ2Constants {
 	public final String ID_DOMANDA_TELEFONO = "17";
 	public final String ID_DOMANDA_TIPO_CONSENSO = "18";
 	public final String ID_DOMANDA_DATA_CONSENSO = "19";
-	
+
 	public final String ID_DOMANDA_PRIMA_VOLTA = "20";
 	public final String ID_DOMANDA_TIPO_PRIMO_SERVIZIO = "21";
 
@@ -28,34 +28,34 @@ public interface DomandeStrutturaQ1AndQ2Constants {
 			+ " \"id\": \"anagraphic-citizen-section\", "
 			+ " \"title\": \"Anagrafica del cittadino\", "
 			+ " \"properties\": ["
-					+ " \"{'%s': ['%s']}\", "
-					+ " \"{'%s': ['%s']}\", "
-					+ " \"{'%s': ['%s']}\", "
-					+ " \"{'%s': ['%s']}\", "
-					+ " \"{'%s': ['%s']}\", "
-					+ " \"{'%s': ['%s']}\", "
-					+ " \"{'%s': ['%s']}\", "
-					+ " \"{'%s': ['%s']}\", "
-					+ " \"{'%s': ['%s']}\", "
-					+ " \"{'%s': ['%s']}\", "
-					+ " \"{'%s': ['%s']}\", "
-					+ " \"{'%s': ['%s']}\", "
-					+ " \"{'%s': ['%s']}\", "
-					+ " \"{'%s': ['%s']}\", "
-					+ " \"{'%s': ['%s']}\", "
-					+ " \"{'%s': ['%s']}\", "
-					+ " \"{'%s': ['%s']}\", "
-					+ " \"{'%s': ['%s']}\", "
-					+ " \"{'%s': ['%s']}\" "
-				+ "]"
+			+ " \"{'%s': ['%s']}\", "
+			+ " \"{'%s': ['%s']}\", "
+			+ " \"{'%s': ['%s']}\", "
+			+ " \"{'%s': ['%s']}\", "
+			+ " \"{'%s': ['%s']}\", "
+			+ " \"{'%s': ['%s']}\", "
+			+ " \"{'%s': ['%s']}\", "
+			+ " \"{'%s': ['%s']}\", "
+			+ " \"{'%s': ['%s']}\", "
+			+ " \"{'%s': ['%s']}\", "
+			+ " \"{'%s': ['%s']}\", "
+			+ " \"{'%s': ['%s']}\", "
+			+ " \"{'%s': ['%s']}\", "
+			+ " \"{'%s': ['%s']}\", "
+			+ " \"{'%s': ['%s']}\", "
+			+ " \"{'%s': ['%s']}\", "
+			+ " \"{'%s': ['%s']}\", "
+			+ " \"{'%s': ['%s']}\", "
+			+ " \"{'%s': ['%s']}\" "
+			+ "]"
 			+ "}";
-	
+
 	public final String SEZIONE_Q2_TEMPLATE = "{ "
 			+ " \"id\": \"anagraphic-booking-section\", "
 			+ " \"title\": \"Anagrafica della prenotazione\", "
 			+ " \"properties\": ["
-					+ " \"{'%s': ['%s']}\", "
-					+ " \"{'%s': ['%s']}\" "
-				+ "]"
+			+ " \"{'%s': ['%s']}\", "
+			+ " \"{'%s': ['%s']}\" "
+			+ "]"
 			+ "}";
 }
--- lib-repdgt-shared/src/main/java/it/pa/repdgt/shared/entity/CittadinoEntity.java
+++ lib-repdgt-shared/src/main/java/it/pa/repdgt/shared/entity/CittadinoEntity.java
@@ -3,16 +3,10 @@ package it.pa.repdgt.shared.entity;
 import java.io.Serializable;
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.validation.constraints.Email;
+import javax.persistence.*;
 
+import it.pa.repdgt.shared.entity.tipologica.FasciaDiEtaEntity;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -20,70 +14,40 @@ import lombok.Setter;
 @Table(name = "cittadino")
 @Setter
 @Getter
-public class CittadinoEntity implements Serializable { 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class CittadinoEntity implements Serializable {
 	private static final long serialVersionUID = -3997184755252624867L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "ID")
+	@EqualsAndHashCode.Include
 	private Long id;
-	
+
 	@Column(name = "CODICE_FISCALE")
 	private String codiceFiscale;
-	
-	@Column(name = "NOME", nullable = false)
-	private String nome;
-	
-	@Column(name = "COGNOME", nullable = false)
-	private String cognome;
-	
+
 	@Column(name = "TIPO_DOCUMENTO")
 	private String tipoDocumento;
 
 	@Column(name = "NUM_DOCUMENTO", unique = true)
 	private String numeroDocumento;
 
-	@Column(name = "GENERE", nullable = true)
+	@Column(name = "GENERE")
 	private String genere;
-	
-	@Column(name = "ANNO_DI_NASCITA", nullable = true)
-	private Integer annoDiNascita;
-	
-	@Column(name = "TITOLO_DI_STUDIO", nullable = true)
+
+	@OneToOne
+	private FasciaDiEtaEntity fasciaDiEta;
+
+	@Column(name = "TITOLO_DI_STUDIO")
 	private String titoloDiStudio;
-	
-	@Column(name = "OCCUPAZIONE", nullable = true)
+
+	@Column(name = "OCCUPAZIONE")
 	private String occupazione;
-	
-	@Column(name = "CITTADINANZA", nullable = true)
+
+	@Column(name = "CITTADINANZA")
 	private String cittadinanza;
-	
-	@Column(name = "COMUNE_DI_DOMICILIO", nullable = true)
-	private String comuneDiDomicilio;
-	
-	@Column(name = "CATEGORIA_FRAGILI", nullable = true)
-	private String categoriaFragili;
-	
-	@Column(name = "EMAIL", nullable = false, unique = true)
-	@Email
-	private String email;
-	
-	@Column(name = "PREFISSO", nullable = true)
-	private String prefissoTelefono;
-	
-	@Column(name = "NUMERO_DI_CELLULARE", nullable = true)
-	private String numeroDiCellulare;
-	
-	@Column(name = "TELEFONO", nullable = true)
-	private String telefono;
-	
-	@Column(name = "TIPO_CONFERIMENTO_CONSENSO", nullable = true)
-	private String tipoConferimentoConsenso;
-	
-	@Column(name = "DATA_CONFERIMENTO_CONSENSO", nullable = true)
-	@Temporal(TemporalType.DATE)
-	private Date dataConferimentoConsenso;
-	
+
 	@Temporal(value = TemporalType.TIMESTAMP)
 	@Column(name = "DATA_ORA_CREAZIONE")
 	private Date dataOraCreazione;
@@ -91,4 +55,7 @@ public class CittadinoEntity implements Serializable {
 	@Temporal(value = TemporalType.TIMESTAMP)
 	@Column(name = "DATA_ORA_AGGIORNAMENTO")
 	private Date dataOraAggiornamento;
+
+	@OneToOne(mappedBy = "cittadino", cascade = CascadeType.ALL)
+	private QuestionarioCompilatoEntity questionarioCompilato;
 }
