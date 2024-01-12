fde219b1b69b6fe8c86859513ff61384d21c5c87
Arturo Buono <a.buono@itdm.it>
2024-01-12 12:09:59 +0100

    feat(fd-shared): updates 20240112

--- lib-repdgt-shared/src/main/java/it/pa/repdgt/shared/entity/QuestionarioCompilatoEntity.java
+++ lib-repdgt-shared/src/main/java/it/pa/repdgt/shared/entity/QuestionarioCompilatoEntity.java
@@ -3,14 +3,7 @@ package it.pa.repdgt.shared.entity;
 import java.io.Serializable;
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.*;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -19,39 +12,39 @@ import lombok.Setter;
 @Table(name = "questionario_compilato")
 @Setter
 @Getter
-public class QuestionarioCompilatoEntity implements Serializable { 
+public class QuestionarioCompilatoEntity implements Serializable {
 	private static final long serialVersionUID = 4720569058596366321L;
 
 	// corrisponde all'id della collection 'questionarioCompilato'
 	@Id
 	@Column(name = "ID")
 	private String id;
-	
+
 	@Column(name = "QUESTIONARIO_TEMPLATE_ID")
 	private String idQuestionarioTemplate;
 
-	@OneToOne(optional = false)
+	@ManyToOne(optional = false)
 	@JoinColumn(name = "ID_CITTADINO", referencedColumnName = "ID")
 	private CittadinoEntity cittadino;
-	
+
 	@Column(name = "FACILITATORE_ID")
 	private String idFacilitatore;
 
 	@Column(name = "SEDE_ID")
 	private Long idSede;
-	
+
 	@Column(name = "ENTE_ID")
 	private Long idEnte;
-	
+
 	@Column(name = "PROGETTO_ID")
 	private Long idProgetto;
-	
+
 	@Column(name = "SERVIZIO_ID")
 	private Long idServizio;
-	
+
 	@Column(name = "STATO")
 	private String stato;
-	
+
 	@Temporal(value = TemporalType.TIMESTAMP)
 	@Column(name = "DATA_ORA_CREAZIONE")
 	private Date dataOraCreazione;
