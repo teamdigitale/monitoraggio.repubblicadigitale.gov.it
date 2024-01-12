25251b1c5ca216349b4d227bf5e82c18278f29a8
Arturo Buono <a.buono@itdm.it>
2024-01-12 17:19:10 +0100

    feat(fd-questionario): updates 20240112

--- ms-questionario-cittadino/src/main/java/it/pa/repdgt/surveymgmt/repository/CittadinoServizioRepository.java
+++ ms-questionario-cittadino/src/main/java/it/pa/repdgt/surveymgmt/repository/CittadinoServizioRepository.java
@@ -11,37 +11,14 @@ import it.pa.repdgt.surveymgmt.projection.CittadinoServizioProjection;
 import it.pa.repdgt.surveymgmt.projection.GetCittadinoProjection;
 
 public interface CittadinoServizioRepository extends JpaRepository<CittadinoEntity, Long> {
-	@Query(value = " "
-			+ " SELECT "
-			+ "	DISTINCT c.id as idCittadino,"
-			+ "			 c.codice_fiscale as codiceFiscale,"
-			+ "             c.data_ora_aggiornamento as dataUltimoAggiornamento,"
-			+ "			 c.num_documento as numeroDocumento,"
-			+ "			 q_c.id as idQuestionario,"
-			+ "			 q_c.stato as statoQuestionario"
-			+ " FROM "
-			+ "	servizio s "
-			+ "	INNER JOIN servizio_x_cittadino s_x_c   "
-			+ "	ON s.ID = s_x_c.id_servizio "
-			+ "	INNER JOIN cittadino c "
-			+ "	ON s_x_c.id_cittadino = c.id"
-			+ "	LEFT JOIN questionario_compilato q_c"
-			+ "	ON q_c.servizio_id = s.id"
-			+ "	AND q_c.id_cittadino = c.id"
-			+ " WHERE 1=1 "
-			+ "    AND s.id = :idServizio"
-			+ "    AND ( "
-			+ "		    :criterioRicercaServizio IS NULL  "
-			+ "		 OR UPPER(c.NUM_DOCUMENTO) = UPPER( :criterioRicercaServizio ) "
-			+ "		 OR UPPER(c.codice_fiscale) = UPPER( :criterioRicercaServizio ) "
-			+ "    ) "
-			+ "    AND ( COALESCE( :statiQuestionariFiltro  ) IS NULL OR q_c.STATO IN ( :statiQuestionariFiltro ) ) "
-			+ "    ORDER BY c.codice_fiscale", nativeQuery = true)
+	@Query(value = "SELECT DISTINCT c.id as idCittadino, c.codice_fiscale as codiceFiscale, c.data_ora_aggiornamento as dataUltimoAggiornamento, c.num_documento as numeroDocumento, q_c.id as idQuestionario, q_c.stato as statoQuestionario FROM servizio s INNER JOIN servizio_x_cittadino s_x_c ON s.ID = s_x_c.id_servizio INNER JOIN cittadino c ON s_x_c.id_cittadino = c.id LEFT JOIN questionario_compilato q_c ON q_c.servizio_id = s.id AND q_c.id_cittadino = c.id WHERE 1=1 AND s.id = :idServizio AND ( :criterioRicercaServizio IS NULL OR UPPER(c.NUM_DOCUMENTO) = UPPER( :criterioRicercaServizio ) OR UPPER(c.codice_fiscale) = UPPER( :criterioRicercaServizio ) ) AND ( COALESCE( :statiQuestionariFiltro ) IS NULL OR q_c.STATO IN ( :statiQuestionariFiltro ) ) ",
+			 nativeQuery = true)
 	List<CittadinoServizioProjection> findAllCittadiniServizioPaginatiByFiltro(
 			@Param(value = "idServizio") Long idServizio,
 			@Param(value = "criterioRicercaServizio") String criterioRicercaServizio,
-			@Param(value = "statiQuestionariFiltro") List<String> statiQuestionariFiltro);
-
+			@Param(value = "statiQuestionariFiltro") List<String> statiQuestionariFiltro
+		);
+	
 	@Query(value = " SELECT "
 			+ "	DISTINCT c.id as idCittadino,"
 			+ "			 c.codice_fiscale as codiceFiscale,"
@@ -63,33 +40,38 @@ public interface CittadinoServizioRepository extends JpaRepository<CittadinoEnti
 			+ "		 OR UPPER(c.NUM_DOCUMENTO) = UPPER( :criterioRicercaServizio ) "
 			+ "		 OR UPPER(c.codice_fiscale) = UPPER( :criterioRicercaServizio ) "
 			+ "    ) "
-			+ "    AND ( COALESCE( :statiQuestionariFiltro  ) IS NULL OR q_c.STATO IN ( :statiQuestionariFiltro ) ) ", nativeQuery = true)
+			+ "    AND ( COALESCE( :statiQuestionariFiltro  ) IS NULL OR q_c.STATO IN ( :statiQuestionariFiltro ) ) ",
+			nativeQuery = true)
 	List<CittadinoServizioProjection> findAllCittadiniServizioByFiltro(
 			@Param(value = "idServizio") Long idServizio,
 			@Param(value = "criterioRicercaServizio") String criterioRicercaServizio,
-			@Param(value = "statiQuestionariFiltro") List<String> statiQuestionariFiltro);
-
+			@Param(value = "statiQuestionariFiltro") List<String> statiQuestionariFiltro
+		);
+	
 	@Query(value = " SELECT "
-			+ "	DISTINCT q_c.STATO"
-			+ " FROM "
-			+ "	servizio s "
-			+ "	INNER JOIN servizio_x_cittadino s_x_c   "
-			+ "	ON s.ID = s_x_c.id_servizio "
-			+ "	INNER JOIN cittadino c "
-			+ "	ON s_x_c.id_cittadino = c.id"
-			+ "	INNER JOIN questionario_compilato q_c"
-			+ "	ON q_c.servizio_id = s.id"
-			+ "	AND q_c.id_cittadino = c.id"
-			+ " WHERE 1=1 "
-			+ "    AND s.id = :idServizio"
-			+ "    AND ( "
-			+ "		    :criterioRicercaServizio IS NULL  "
-			+ "		 OR UPPER(c.NUM_DOCUMENTO) = UPPER( :criterioRicercaServizio ) "
-			+ "		 OR UPPER(c.codice_fiscale) = UPPER( :criterioRicercaServizio ) "
-			+ "    ) ", nativeQuery = true)
+			 + "	DISTINCT q_c.STATO"
+			 + " FROM "
+			 + "	servizio s "
+			 + "	INNER JOIN servizio_x_cittadino s_x_c   "
+			 + "	ON s.ID = s_x_c.id_servizio "
+			 + "	INNER JOIN cittadino c "
+			 + "	ON s_x_c.id_cittadino = c.id"
+			 + "	INNER JOIN questionario_compilato q_c"
+			 + "	ON q_c.servizio_id = s.id"
+			 + "	AND q_c.id_cittadino = c.id"
+			 + " WHERE 1=1 "
+			 + "    AND s.id = :idServizio"
+			 + "    AND ( "
+			 + "		    :criterioRicercaServizio IS NULL  "
+		     + "		 OR UPPER(c.NUM_DOCUMENTO) = UPPER( :criterioRicercaServizio ) "
+		     + "		 OR UPPER(c.codice_fiscale) = UPPER( :criterioRicercaServizio ) "
+	         + "    ) "
+	         ,
+			 nativeQuery = true)
 	List<String> getAllStatiQuestionarioCittadinoServizioDropdown(
 			@Param(value = "idServizio") Long idServizio,
-			@Param(value = "criterioRicercaServizio") String criterioRicercaServizio);
+			@Param(value = "criterioRicercaServizio") String criterioRicercaServizio
+			);
 
 	@Query(value = "    "
 			+ " SELECT  "
@@ -101,7 +83,8 @@ public interface CittadinoServizioRepository extends JpaRepository<CittadinoEnti
 			+ " WHERE 1=1     "
 			+ "		AND ((UPPER(:tipoDocumento) = 'CF' AND UPPER(codice_fiscale) = UPPER(:criterioRicerca))       "
 			+ " 	OR (UPPER(:tipoDocumento) = 'NUM_DOC' AND UPPER(num_documento) = UPPER(:criterioRicerca))) "
-			+ " ORDER BY codice_fiscale", nativeQuery = true)
+			+ " ORDER BY codice_fiscale",
+			 nativeQuery = true)
 	List<GetCittadinoProjection> getAllCittadiniByCodFiscOrNumDoc(@Param(value = "tipoDocumento") String tipoDocumento,
 			@Param(value = "criterioRicerca") String criterioRicerca);
 }
