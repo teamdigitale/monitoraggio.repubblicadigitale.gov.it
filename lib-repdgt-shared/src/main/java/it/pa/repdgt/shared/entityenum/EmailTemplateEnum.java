97a11a36d72226b6da33c5e46846a71e8f92aef2
Arturo Buono <a.buono@itdm.it>
2024-04-08 16:48:03 +0200

    fix(email): updates 20240408

--- lib-repdgt-shared/src/main/java/it/pa/repdgt/shared/entityenum/EmailTemplateEnum.java
+++ lib-repdgt-shared/src/main/java/it/pa/repdgt/shared/entityenum/EmailTemplateEnum.java
@@ -4,17 +4,16 @@ import lombok.Getter;
 
 @Getter
 public enum EmailTemplateEnum {
-	 CONSENSO("templ_consenso","Presa visione dell’informativa privacy – Repubblica digitale")
-	,GEST_PROG("templ_gestore_programma","Ti diamo il benvenuto su Repubblica Digitale")
-	,GEST_PROGE_PARTNER("templ_gest_prog_and_partner","Ti diamo il benvenuto su Repubblica Digitale")
-	,FACILITATORE("templ_facilitatore","Ti diamo il benvenuto su Repubblica Digitale")
-	,RUOLO_CUSTOM("templ_custom","Ti diamo il benvenuto su Repubblica Digitale")
-	,QUESTIONARIO_ONLINE("templ_quest_online","%s - Compila il questionario di Repubblica Digitale")
-	;
-	
+	CONSENSO("templ_consenso", "Presa visione dell’informativa privacy – Facilita"),
+	GEST_PROG("templ_gestore_programma", "Ti diamo il benvenuto su Facilita"),
+	GEST_PROGE_PARTNER("templ_gest_prog_and_partner", "Ti diamo il benvenuto su Facilita"),
+	FACILITATORE("templ_facilitatore", "Ti diamo il benvenuto su Facilita"),
+	RUOLO_CUSTOM("templ_custom", "Ti diamo il benvenuto su Facilita"),
+	QUESTIONARIO_ONLINE("templ_quest_online", "%s - Compila il questionario di Facilita");
+
 	private String valueTemplate;
 	private String valueTemplateSubject;
-	
+
 	private EmailTemplateEnum(String valueTemplate, String valueTemplateSubject) {
 		this.valueTemplate = valueTemplate;
 		this.valueTemplateSubject = valueTemplateSubject;
