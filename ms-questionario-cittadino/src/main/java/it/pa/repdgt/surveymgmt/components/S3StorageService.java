73bed0d0f2f10bfcc434fbfe6e75a4d3e60c65ee
Arturo Buono <a.buono@itdm.it>
2024-06-10 13:52:30 +0200

    fix(be-logs): updates 20240610

--- ms-questionario-cittadino/src/main/java/it/pa/repdgt/surveymgmt/components/S3StorageService.java
+++ ms-questionario-cittadino/src/main/java/it/pa/repdgt/surveymgmt/components/S3StorageService.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.net.URL;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -21,20 +22,26 @@ public class S3StorageService {
     private final S3PropertiesConfig s3PropertiesConfig;
 
     public String generateUrl(String fileName, HttpMethod httpMethod, String bucketName) {
+        log.debug("bucketName {} e fileName {}", bucketName, fileName);
+        log.info("bucketName {} e fileName {}", bucketName, fileName);
         if (stripSOPrefix(fileName) != null && stripSOPrefix(fileName).equals("")) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(new Date());
         calendar.add(Calendar.MINUTE, s3PropertiesConfig.getExpirationTimeInMinutes());
-        return amazonS3.generatePresignedUrl(bucketName, fileName, calendar.getTime(), httpMethod).toString();
+        String url = amazonS3.generatePresignedUrl(bucketName, fileName, calendar.getTime(), httpMethod).toString();
+        log.debug("url generato {}", url);
+        log.info("url generato {}", url);
+        return url;
     }
+
 
     public String save(String fileName, String bucketName) {
         return generateUrl(fileName, HttpMethod.PUT, bucketName);
     }
 
-    public String stripSOPrefix(String filePath) {
-        return filePath.replaceAll("\\d+\\/", "");
+    public String stripSOPrefix(String filePath){
+        return filePath.replaceAll("\\d+\\/","");
     }
 }
