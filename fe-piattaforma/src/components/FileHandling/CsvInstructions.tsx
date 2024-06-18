af83392135140092116b8b86dd618e5eb16c3be3
Arturo Buono <a.buono@itdm.it>
2024-06-18 17:32:50 +0200

    fix(csv-link): updates 20240618

--- fe-piattaforma/src/components/FileHandling/CsvInstructions.tsx
+++ fe-piattaforma/src/components/FileHandling/CsvInstructions.tsx
@@ -1,25 +1,7 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import uploadIcon from '../../../public/assets/img/it-upload-primary.png';
-import { downloadCSVGuide } from '../../services/activityReportService';
-import { dispatchNotify } from '../../utils/notifictionHelper';
 
-function showErrorDownloadGuide() {
-  dispatchNotify({
-    title: 'Guida utente',
-    status: 'error',
-    message: 'Impossibile scaricare il file, permessi mancanti',
-    closable: true,
-    duration: 'slow',
-  });
-}
 export default function CsvInstructions() {
-  const onDownloadGuide = useCallback(() => {
-    downloadCSVGuide().catch(() => {
-      showErrorDownloadGuide();
-    });
-  }, []);
-
-
   return (
     <div className='d-flex flex-column'>
       <h1 className='h5 text-black font-weight-semibold'>
@@ -62,9 +44,9 @@ export default function CsvInstructions() {
           file e leggere il registro dei caricamenti.
         </p>
         <a
-          href="https://s3-mitd-drupal-prod.s3.eu-central-1.amazonaws.com/public/2024-06/Guida_Operativa_Caricamento_Dati_17186481766690424010.pdf?versionId=4inbSdqj3DYQ4gcKM3TlaldlGFxIfqyy&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA6RGETSJQ7K6J4KNR%2F20240618%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20240618T043638Z&X-Amz-SignedHeaders=host&X-Amz-Expires=7200&X-Amz-Signature=631b8710c7faa27ffaa47869924abe6647828fc245f389d64fb1067884307762"
-          target="_blank"
-          rel="noreferrer"
+          href='/assets/pdf/guida-operativa-csv.pdf'
+          download='guida-operativa-csv.pdf'
+          target='_blank'
           className='btn p-0 text-primary-action'
         >
           <img className='delete-csv-btn-img mr-2' src={uploadIcon} alt='' />
