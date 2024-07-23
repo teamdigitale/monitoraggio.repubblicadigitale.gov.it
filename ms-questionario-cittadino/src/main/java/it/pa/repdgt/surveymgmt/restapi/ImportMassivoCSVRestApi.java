bfa18f90d3b58c39c32e0e7918edb7495f24736d
salvatore.gravina <salvatore.gravina@dxc.com>
2024-07-23 18:22:52 +0200

    fix(fe-piattaforma): fix chiamata importCsv 20240723

--- ms-questionario-cittadino/src/main/java/it/pa/repdgt/surveymgmt/restapi/ImportMassivoCSVRestApi.java
+++ ms-questionario-cittadino/src/main/java/it/pa/repdgt/surveymgmt/restapi/ImportMassivoCSVRestApi.java
@@ -5,7 +5,6 @@ import it.pa.repdgt.surveymgmt.model.ElaboratoCSVRequest;
 import it.pa.repdgt.surveymgmt.model.ImportCsvInputData;
 import it.pa.repdgt.surveymgmt.service.ImportMassivoCSVService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 import org.springframework.http.HttpStatus;
