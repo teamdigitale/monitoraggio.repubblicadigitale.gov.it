1d5dbaa3b649f6c5b087ec65e1d624d470c3c1ec
Matteo Grifantini <matteo.grifantini@dxc.com>
2024-10-04 18:16:04 +0200

    fix(monitoraggio-caricamenti): WIP gestione select filtri 20241004

--- fe-piattaforma/src/pages/administrator/MonitoraggioCaricamenti/monitoringSearchFilters.tsx
+++ fe-piattaforma/src/pages/administrator/MonitoraggioCaricamenti/monitoringSearchFilters.tsx
@@ -1,8 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from 'design-react-kit';
 import { Form, Input, Select } from '../../../components';
 import withFormHandler, { withFormHandlerProps } from '../../../hoc/withFormHandler';
 import { formFieldI, newForm, newFormField } from '../../../utils/formHelper';
+import { Select as SelectKit, SelectProps } from 'design-react-kit';
+import clsx from 'clsx';
+
+export type OptionType = {
+  value: string | number;
+  label: string;
+};
 
 interface MonitoringSearchFilterI {
   formDisabled?: boolean;
@@ -11,54 +18,73 @@ interface MonitoringSearchFilterI {
   clearForm?: () => void;
 }
 
-const initialFormValues = {
-  ente: '',
-  programma: '',
-  intervento: '',
-  progetto: '',
-  dataInizio: new Date().toISOString().split('T')[0],
-  dataFine: new Date().toISOString().split('T')[0],
+type DateField = {
+  value: any;
+  minimum?: string;
+  maximum?: string;
 };
 
-const formSchema: { [key: string]: formFieldI } = newForm([
-  newFormField({
-    field: 'ente',
-    id: 'ente',
-    required: false,
-    maximum: 55,
-    minimum: 2,
-  }),
-  newFormField({
-    field: 'intervento',
-    id: 'intervento',
-    type: 'select',
-    required: false,
-  }),
-  newFormField({
-    field: 'programma',
-    id: 'programma',
-    type: 'select',
-    required: false,
-  }),
-  newFormField({
-    field: 'progetto',
-    id: 'progetto',
-    type: 'select',
-    required: false,
-  }),
-  newFormField({
-    field: 'dataInizio',
-    id: 'dataInizio',
-    type: 'date',
-    required: false,
-  }),
-  newFormField({
-    field: 'dataFine',
-    id: 'dataFine',
-    type: 'date',
-    required: false,
-  }),
-]);
+const initialFormValues = {
+  ente: '',
+  programma: {
+    value: '',
+    label: 'Seleziona',
+  } as OptionType,
+  intervento: {
+    value: '',
+    label: 'Seleziona',
+  } as OptionType,
+  progetto: {
+    value: '',
+    label: 'Seleziona',
+  } as OptionType,
+  dataInizio: {
+    value: new Date().toISOString().split('T')[0],
+  } as DateField,
+  dataFine: {
+    value: new Date().toISOString().split('T')[0],
+  } as DateField,
+};
+
+// const formSchema: { [key: string]: formFieldI } = newForm([
+//   newFormField({
+//     field: 'ente',
+//     id: 'ente',
+//     required: false,
+//     maximum: 55,
+//     minimum: 2,
+//   }),
+//   newFormField({
+//     field: 'intervento',
+//     id: 'intervento',
+//     type: 'select',
+//     required: false,
+//   }),
+//   newFormField({
+//     field: 'programma',
+//     id: 'programma',
+//     type: 'select',
+//     required: false,
+//   }),
+//   newFormField({
+//     field: 'progetto',
+//     id: 'progetto',
+//     type: 'select',
+//     required: false,
+//   }),
+//   newFormField({
+//     field: 'dataInizio',
+//     id: 'dataInizio',
+//     type: 'date',
+//     required: false,
+//   }),
+//   newFormField({
+//     field: 'dataFine',
+//     id: 'dataFine',
+//     type: 'date',
+//     required: false,
+//   }),
+// ]);
 
 const MonitoringSearchFilters: React.FC<MonitoringSearchFilterI> = ({
   // sendNewValues = () => {},
@@ -66,20 +92,27 @@ const MonitoringSearchFilters: React.FC<MonitoringSearchFilterI> = ({
   // updateForm = () => {},
   // clearForm = () => {},
 }) => {
-  const [formValues, setFormValues] = useState<{ [key: string]: formFieldI['value'] }>(initialFormValues);
+  const isDisabled = false;
+  const shortDropdownMenu = false;
+  const isSearchable = false;
+  const withLabel = true;
+  const required = false;
+  const [formValues, setFormValues] = useState<typeof initialFormValues>(initialFormValues);
 
-  const handleInputChange = (value: formFieldI['value'], field: formFieldI['field']) => {
+  const handleInputChange = (value: formFieldI['value'], field: string) => {
     console.log('field prima', field);
     console.log('value prima', value);
     console.log('formValues prima', formValues);
     
     setFormValues((prevValues) => {
-      const newValues = { ...prevValues, [field]: value };
-      console.log('prevValues', prevValues);
-      console.log('newValues', newValues);
+      const newValues = { ...prevValues, [field]: value as string | number | boolean | Date | OptionType };
       return newValues;
     });
 
+  };
+
+  const handleSelectChange = (option: OptionType, name: any) => {
+    setFormValues((prevValues) => ({ ...prevValues, [name?.name]: option }));
   };
 
   const handleClearForm = () => {
@@ -88,20 +121,146 @@ const MonitoringSearchFilters: React.FC<MonitoringSearchFilterI> = ({
     // clearForm();
   };
 
-  const handleSubmit = () => {
-    // sendNewValues(formValues);
-    onSearch(formValues);
-  };
+  // GESTIONE DATE DA RIVEDERE
+      
+  //  useEffect(() => {
+  //   // Verifica se le date sono state passate tramite i props
+  //   if (formValues.dataInizio && formValues.dataFine) {
+  //     let newForm = { ...formValues };
+  //     const dataInizio = new Date(formValues.dataInizio.value);
+  //     const dataFine = new Date(formValues.dataFine.value);
+  
+  //     // Imposta il minimo per dataFine basato su dataInizio
+  //     if (dataInizio && (!newForm.dataFine.minimum || new Date(newForm.dataFine.minimum) < dataInizio)) {
+  //       newForm.dataFine.minimum = dataInizio.toISOString().split('T')[0];
+  //     }
+  
+  //     // Imposta il massimo per dataInizio basato su dataFine
+  //     if (dataFine && (!newForm.dataInizio.maximum || new Date(newForm.dataInizio.maximum) > dataFine)) {
+  //       newForm.dataInizio.maximum = dataFine.toISOString().split('T')[0];
+  //     }
+  
+  //     //updateForm(newForm); // Aggiorna il form con i valori min/max impostati
+  //     setFormValues(newForm); // Imposta i valori del form
+  //   }
+  // }, [formValues.dataInizio, formValues.dataFine]);
 
-  const renderSelect = (field: string, label: string, options: Array<{ value: string; label: string }>) => (
-    <Select
-      col="col-12 col-lg-6"
-      label={label}
-      onInputChange={(value) => handleInputChange(value, field)}
-      options={options}
-      field={field}
-      value={formValues[field]} // Usa i valori attuali dello stato
-    />
+  // const onDateChange = (
+  //   value: formFieldI['value'],
+  //   field?: formFieldI['field']
+  // ) => {
+  //   if (value && field && formValues) {
+  //     // Verifica se il valore è una stringa o un numero prima di usarlo in new Date()
+  //     const isValidDateValue =
+  //       typeof value === 'string' || typeof value === 'number' || value instanceof Date;
+  
+  //     if (!isValidDateValue) {
+  //       console.error('Valore non valido per la data:', value);
+  //       return; // Interrompe l'esecuzione se il valore non è valido
+  //     }
+  //     const formattedDate = formatDate(value as string); // Converti la data in formato YYYY-MM-DD
+  //     let newForm = { ...formValues};
+  
+  //     if (field === 'dataInizio') {
+  //       // Controlla se dataFine è inferiore a dataInizio
+  //       if (newForm.dataFine.value && (typeof newForm.dataFine.value === 'string' || typeof newForm.dataFine.value === 'number' || newForm.dataFine.value instanceof Date) && new Date(newForm.dataFine.value) < new Date(formattedDate)) {
+  //         newForm.dataFine.value = ''; // Resetta dataFine se è inferiore a dataInizio
+  //       }
+  //       newForm = {
+  //         ...newForm,
+  //         dataInizio: {
+  //           ...formValues.dataInizio,
+  //           value: formattedDate,
+  //         },
+  //         dataFine: {
+  //           ...formValues.dataFine,
+  //           minimum: formattedDate,
+  //         },
+  //       };
+  //     } else if (field === 'dataFine') {
+  //       // Controlla se dataFine è inferiore a dataInizio
+  //       if (newForm.dataInizio.value && new Date(formattedDate) < new Date(newForm.dataInizio.value as string | number | Date)) {
+  //         console.error('La data di fine non può essere inferiore alla data di inizio');
+  //         return;
+  //       }
+  //       newForm = {
+  //         ...newForm,
+  //         dataFine: {
+  //           ...formValues.dataFine,
+  //           value: formattedDate,
+  //         },
+  //         dataInizio: {
+  //           ...formValues.dataInizio,
+  //           maximum: formattedDate,
+  //         },
+  //       };
+  //     }
+  //     setFormValues(newForm);
+  //   }
+  // };
+  
+  //   const formatDate = (dateString: string) => {
+  //     const date = new Date(dateString);
+  //     const year = date.getFullYear();
+  //     const month = String(date.getMonth() + 1).padStart(2, '0');
+  //     const day = String(date.getDate()).padStart(2, '0');
+  //     return `${year}-${month}-${day}`;
+  //   };
+
+  const renderSelect = (
+    field: keyof typeof initialFormValues,
+    label: string,
+    options: Array<{ value: string; label: string }>
+  ) => (
+    // <Select
+    //   col="col-12 col-lg-6"
+    //   label={label}
+    //   onInputChange={(value) => handleInputChange(value, field)}
+    //   options={options}
+    //   field={field}
+    //   value={formValues[field]} // Usa i valori attuali dello stato
+    // />
+    <div
+      className={clsx(
+        'bootstrap-select-wrapper',
+        'form-group',
+        'col-12 col-lg-6',
+        // wrapperClassName
+      )}
+    >
+      {withLabel ? (
+        <label
+          id={`${(label || 'label select').replace(/\s/g, '-')}`}
+          className='text-decoration-none'
+        >
+          {label}
+          {required && !isDisabled ? ' *' : ''}
+        </label>
+      ) : null}
+      <SelectKit
+        // {...BaseProps}
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        onChange={handleSelectChange}
+        name={field}
+        id={field}
+        options={options}
+        value={formValues[field]}
+        menuPlacement={'auto'}
+        //onMenuScrollToBottom={onMenuScrollToBottom}
+        // color='primary'
+        className={clsx(
+          (formValues[field] && !isDisabled ? 'border-select-value' : '') ||
+            (isDisabled ? 'deleteArrowInSelect' : '')
+        )}
+        classNamePrefix={clsx(
+          shortDropdownMenu ? 'bootstrap-select-short' : 'bootstrap-select'
+        )}
+        aria-labelledby={`${(label || 'label select').replace(/\s/g, '-')}`}
+        isDisabled={isDisabled}
+        isSearchable={isSearchable}
+      />
+    </div>
   );
 
   return (
@@ -123,14 +282,14 @@ const MonitoringSearchFilters: React.FC<MonitoringSearchFilterI> = ({
 
       <Form.Row className="justify-content-between px-0 px-lg-5 mx-2">
         <Input
-          value={formValues.dataInizio} // Usa il valore attuale dello stato
+          value={formValues.dataInizio.value} // Usa il valore attuale dello stato
           type="date"
           col="col-12 col-lg-6"
           label="Data inizio"
           onInputChange={(value) => handleInputChange(value, 'dataInizio')}
         />
         <Input
-          value={formValues.dataFine} // Usa il valore attuale dello stato
+          value={formValues.dataFine.value} // Usa il valore attuale dello stato
           type="date"
           col="col-12 col-lg-6"
           label="Data fine"
@@ -155,7 +314,7 @@ const MonitoringSearchFilters: React.FC<MonitoringSearchFilterI> = ({
         <Button color="secondary" className="mr-2" onClick={handleClearForm}>
           Cancella filtri
         </Button>
-        <Button color="primary" onClick={handleSubmit}>
+        <Button color="primary" /*onClick={handleSubmit}*/>
           Applica filtri
         </Button>
       </Form.Row>
