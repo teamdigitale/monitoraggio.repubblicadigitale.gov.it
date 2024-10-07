d4151bd0054c8a06b8bd3fdd3738a88321e4d492
federico-tocci-dxc <federico.tocci@dxc.com>
2024-10-07 17:53:12 +0200

    feat(monitoraggio-caricamenti): creazione endpoint BE recupero dati tabella 20241007

---
+++ lib-repdgt-shared/src/main/java/it/pa/repdgt/shared/entity/VMonitoraggioCaricamentiEntity.java
@@ -0,0 +1,59 @@
+package it.pa.repdgt.shared.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "v_monitoraggio_caricamenti")
+@Setter
+@Getter
+public class VMonitoraggioCaricamentiEntity {
+
+    @Id
+    @Column(name = "id_programma", nullable = false)
+    private Long idProgramma;
+
+    @Column(name = "intervento")
+    private String intervento;
+
+    @Column(name = "nome_programma", nullable = false)
+    private String nomeProgramma;
+
+    @Column(name = "id_progetto", nullable = false)
+    private Long idProgetto;
+
+    @Column(name = "nome_progetto",  nullable = false)
+    private String nomeProgetto;
+
+    @Column(name = "id_ente", nullable = false)
+    private Long idEnte;
+
+    @Column(name = "nome_ente")
+    private String nomeEnte;
+
+    @Column(name = "data_caricamenti")
+    @Temporal(TemporalType.DATE)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Date dataCaricamenti;
+
+    @Column(name = "num_caricamenti", nullable = false)
+    private Long numCaricamenti;
+
+    @Column(name = "servizi_aggiunti")
+    private Long serviziAggiunti;
+
+    @Column(name = "cittadini_associati")
+    private Long cittadiniAssociati;
+    
+}
---
+++ ms-questionario-cittadino/src/main/java/it/pa/repdgt/surveymgmt/request/MonitoraggioCaricamentiRequest.java
@@ -0,0 +1,34 @@
+package it.pa.repdgt.surveymgmt.request;
+
+import java.sql.Date;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class MonitoraggioCaricamentiRequest {
+
+	@JsonProperty("intervento")
+	private String intervento;
+
+	@JsonProperty("idProgetto")
+	private Long idProgetto; 
+
+	@JsonProperty("idProgramma")
+	private Long idProgramma; 
+
+	@JsonProperty("idEnti")
+	private List<Long> idEnti;
+
+	@JsonProperty("dataInizio")
+	private Date dataInizio;
+
+	@JsonProperty("dataFine")
+	private Date dataFine;
+}
