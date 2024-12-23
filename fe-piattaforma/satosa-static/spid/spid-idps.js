91db89c78cd6f4019bbe74b591ab3db560306e79
sgravina <salvatore.gravina@dxc.com>
2024-12-23 10:04:31 +0100

    feat(SPID): nuova versione tasto spid 20241223

--- fe-piattaforma/satosa-static/spid/spid-idps.js
+++ fe-piattaforma/satosa-static/spid/spid-idps.js
@@ -1,48 +1,67 @@
-// * spid-idps.js *
-// This script populate the SPID button with the SPID IDPS
-//
-// ** Configuration ***
-// const idps define list of SPID IDPs
-// - entityName - string with IDP name
-// - entityID - string with IDP entityID
-// - logo - url of IDP logo image
-const idps = [
-  {"entityName": "SPID Test", "entityID": "https://spid-check.facilitazione-test.repubblicadigitale.gov.it/demo", "logo": ""},
-  {"entityName": "Aruba ID", "entityID": "https://loginspid.aruba.it", "logo": "spid/spid-idp-arubaid.svg"},
-  {"entityName": "Infocert ID", "entityID": "https://identity.infocert.it", "logo": "spid/spid-idp-infocertid.svg"},
-  {"entityName": "Intesa ID", "entityID": "https://spid.intesa.it", "logo": "spid/spid-idp-intesaid.svg"},
-  {"entityName": "Lepida ID", "entityID": "https://id.lepida.it/idp/shibboleth", "logo": "spid/spid-idp-lepidaid.svg"},
-  {"entityName": "Namirial ID", "entityID": "https://idp.namirialtsp.com/idp", "logo": "spid/spid-idp-namirialid.svg"},
-  {"entityName": "Poste ID", "entityID": "https://posteid.poste.it", "logo": "spid/spid-idp-posteid.svg"},
-  {"entityName": "Sielte ID", "entityID": "https://identity.sieltecloud.it", "logo": "spid/spid-idp-sielteid.svg"},
-  {"entityName": "SPIDItalia Register.it", "entityID": "https://spid.register.it", "logo": "spid/spid-idp-spiditalia.svg"},
-  {"entityName": "Tim ID", "entityIDD": "https://login.id.tim.it/affwebservices/public/saml2sso", "logo": "spid/spid-idp-timid.svg"}
-].sort(() => Math.random() - 0.5)
 
-// ** Values **
+// * spid-idps.js *
+// This script populate the SPID button with a remote json or the `idps` var content
+//var queryURL = "js/JSON_IDP_list_EXAMPLE.json";
+var queryURL = "https://registry.spid.gov.it/entities-idp?&output=json&custom=info_display_base";
+var idps = [{"organization_name": "ArubaPEC S.p.A.", "entity_id": "https://loginspid.aruba.it", "logo_uri": "img/spid-idp-arubaid.svg"},{"organization_name": "InfoCert S.p.A.", "entity_id": "https://identity.infocert.it", "logo_uri": "img/spid-idp-infocertid.svg"},{"organization_name": "IN.TE.S.A. S.p.A.", "entity_id": "https://spid.intesa.it", "logo_uri": "img/spid-idp-intesaid.svg"},{"organization_name": "Lepida S.p.A.", "entity_id": "https://id.lepida.it/idp/shibboleth", "logo_uri": "img/spid-idp-lepidaid.svg"},{"organization_name": "Namirial", "entity_id": "https://idp.namirialtsp.com/idp", "logo_uri": "img/spid-idp-namirialid.svg"},{"organization_name": "Poste Italiane SpA", "entity_id": "https://posteid.poste.it", "logo_uri": "img/spid-idp-posteid.svg"},{"organization_name": "Sielte S.p.A.", "entity_id": "https://identity.sieltecloud.it", "logo_uri": "img/spid-idp-sielteid.svg"},{"organization_name": "Register.it S.p.A.", "entity_id": "https://spid.register.it", "logo_uri": "img/spid-idp-spiditalia.svg"},{"organization_name": "TI Trust Technologies srl", "entity_id": "https://login.id.tim.it/affwebservices/public/saml2sso", "logo_uri": "img/spid-idp-timid.svg"},{"organization_name": "TeamSystem s.p.a.", "entity_id": "https://spid.teamsystem.com/idp", "logo_uri": "img/spid-idp-teamsystemid.svg"}]
+
+// spid_populate function, if '.spid-button[data-spid-remote] ul' exist, try to get the remote json file and pupulate all spid buttons
+function spid_populate() {
+  let spid_elements = document.querySelectorAll('ul[data-spid-remote]')
+  console.log("spid_elements",spid_elements)
+
+  if (spid_elements.length > 0 ) {
+    fetch(queryURL)
+     .then(function (response) {
+       return response.json();
+      })
+     .then(function (idps) {
+      // aggiunta dello SPID Test
+      idps.push({"organization_name": "SPID Test", "entity_id": "https://spid-check.facilitazione-test.repubblicadigitale.gov.it/demo", "logo_uri": ""})
+       idps = idps.sort(() => Math.random() - 0.5)
+       for (var u = 0; u < spid_elements.length; u++) {
+	      for (var i = 0; i < idps.length; i++) { spid_addIdpEntry(idps[i], spid_elements[u]); }
+
+       }
+      })
+     .catch(function (error) {
+      console.log('Error during fetch: ' + error.message);
+      idps.sort(() => Math.random() - 0.5)
+      for (var u = 0; u < spid_elements.length; u++) {
+        for (var i = 0; i < idps.length; i++) { spid_addIdpEntry(idps[i], spid_elements[u]); }
+	    }
+     });
+  }
+}
+
 const urlParams = new URLSearchParams(window.location.search);
 const servicePath = urlParams.get("return");
 const entityID = urlParams.get('entityID');
 
-// function addIdpEntry make a "li" element with the ipd link and prepend this in a element
+// function spid_addIdpEntry make a "li" element with the ipd link and prepend this in a element
 //
 // options:
-// - data - is an object with "entityName", "entityID" and "logo" values
+// - data - is an object with "organization_name", "entity_id" and "logo_uri" values
 // - element - is the element where is added the new "li" element
-function addIdpEntry(data, element) {
+function spid_addIdpEntry(data, element) {
+  const att = document.createAttribute("data-idp");
+  att.value = data['entity_id'];
   let li = document.createElement('li');
-  li.className = 'spid-idp-button-link'
-  li.innerHTML = `<a href="${servicePath}?entityID=${data['entityID']}&return=${servicePath}"><span class="spid-sr-only">${data['entityName']}</span><img src="${data['logo']}" alt="${data['entityName']}"></a>`
+  li.className = 'spid-idp-button-link';
+  li.setAttributeNode(att);
+  if (element.id.indexOf('post')!== -1) {
+    li.innerHTML = `<button class="idp-button-idp-logo" name="${data['organization_name']}" type="submit"><span class="spid-sr-only">${data['organization_name']}</span><img class="spid-idp-button-logo" src="${data['logo_uri']}" alt="${data['organization_name']}" /></button></li>`
+  };  
+  if (element.id.indexOf('get')!== -1) {
+    li.innerHTML = `<a href="${servicePath}?entityID=${data['entity_id']}&return=${servicePath}"><span class="spid-sr-only">${data['organization_name']}</span><img src="${data['logo_uri']}" alt="${data['organization_name']}" /></a>`
+  };
   element.prepend(li)
 }
 
-// when page is ready add each idps entry in the ul element
+// when page is ready populate all spid buttons
 document.onreadystatechange = function () {
   if (document.readyState == "interactive") {
-    // user alert if the page is loaded without entityID param
-    if (! entityID ) { alert('To use a Discovery Service you must come from a Service Provider') }
-    // var ul define the contain of ipds link
-    var ul = document.querySelector('ul#spid-idp-list-medium-root-get');
-    for (var i = 0; i < idps.length; i++) { addIdpEntry(idps[i], ul); }
+    spid_populate()
   }
 }
+
