7fb35e6edc0a6996ec2447cd56a84a195398d87c
federico-tocci-dxc <federico.tocci@dxc.com>
2025-02-27 15:54:45 +0100

    feat(U250): fix refresh liste e cambio label bottoni conferma 20250227

--- fe-piattaforma/src/pages/administrator/AdministrativeArea/Entities/Headquarters/HeadquartersDetail/headquartersDetails.tsx
+++ fe-piattaforma/src/pages/administrator/AdministrativeArea/Entities/Headquarters/HeadquartersDetail/headquartersDetails.tsx
@@ -181,7 +181,7 @@ const HeadquartersDetails = () => {
     }
   }, [headquarterId, projectId, authorityId]);
 
-  const itemAccordionList: ItemsListI[] = [
+  let itemAccordionList: ItemsListI[] = [
     {
       title: 'Facilitatori',
       items:
@@ -213,6 +213,41 @@ const HeadquartersDetails = () => {
         })) || [],
     },
   ];
+
+  function updateFacilitators(){
+    itemAccordionList = [
+      {
+        title: 'Facilitatori',
+        items:
+          headquarterfacilitators?.map((facilitator: HeadquarterFacilitator) => ({
+            nome: `${facilitator.cognome} ${facilitator.nome} `,
+            stato: facilitator.stato,
+            actions: facilitator.associatoAUtente
+              ? facilitator.stato === entityStatus.ATTIVO &&
+                authorityType &&
+                (((authorityType === formTypes.ENTE_GESTORE_PROGRAMMA ||
+                  authorityType === formTypes.ENTE_GESTORE_PROGETTO) &&
+                  hasUserPermission(['add.fac'])) ||
+                  (authorityType === formTypes.ENTI_PARTNER &&
+                    hasUserPermission(['add.fac.partner'])))
+                ? onActionClick
+                : {
+                    [CRUDActionTypes.VIEW]:
+                      ((authorityType === formTypes.ENTE_GESTORE_PROGRAMMA ||
+                        authorityType === formTypes.ENTE_GESTORE_PROGETTO) &&
+                        hasUserPermission(['add.fac'])) ||
+                      (authorityType === formTypes.ENTI_PARTNER &&
+                        hasUserPermission(['add.fac.partner']))
+                        ? onActionClick[CRUDActionTypes.VIEW]
+                        : undefined,
+                  }
+              : {},
+            id: facilitator?.id,
+            codiceFiscale: facilitator?.codiceFiscale,
+          })) || [],
+      },
+    ];
+  }
 
   let buttons: ButtonInButtonsBar[] = [];
 
@@ -435,7 +470,7 @@ const HeadquartersDetails = () => {
             </div>
           </Sticky>
           <ManageHeadquarter />
-          <ManageFacilitator creation />
+          <ManageFacilitator creation fromProject={true} updateFacilitators={updateFacilitators}/>
           <DeleteEntityModal
             onClose={() => dispatch(closeModal())}
             onConfirm={(payload) => {
