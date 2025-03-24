7bf411b3acd619bc80ff85a721c11bffdf62e018
lcucuzzella <lorenzo.cucuzzella@dxc.com>
2025-03-24 15:10:30 +0100

    feat(U385): prima implementazione BE controllo cittadini minorenni 20250324

---
+++ ms-questionario-cittadino/src/main/java/it/pa/repdgt/surveymgmt/dto/ConfigurazioneMinorenniDto.java
@@ -0,0 +1,20 @@
+package it.pa.repdgt.surveymgmt.dto;
+
+import java.util.Date;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ConfigurazioneMinorenniDto {
+    private Long id;
+    private Long idProgramma;
+    private String intervento;
+    private Date dataAbilitazione;
+    private Date dataDecorrenza;
+    private Date createTimestamp;
+    private String createUser;
+    private Date updateTimestamp;
+    private String updateUser;
+}
---
+++ ms-questionario-cittadino/src/main/java/it/pa/repdgt/surveymgmt/service/ConfigurazioneMinorenniService.java
@@ -0,0 +1,44 @@
+package it.pa.repdgt.surveymgmt.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import it.pa.repdgt.shared.annotation.LogExecutionTime;
+import it.pa.repdgt.shared.annotation.LogMethod;
+import it.pa.repdgt.surveymgmt.dto.ConfigurazioneMinorenniDto;
+import it.pa.repdgt.surveymgmt.repository.ConfigurazioneMinorenniRepository;
+
+@Service
+public class ConfigurazioneMinorenniService {
+
+    @Autowired
+    private ConfigurazioneMinorenniRepository configurazioneMinorenniRepository;
+
+    @LogMethod
+    @LogExecutionTime
+    public ConfigurazioneMinorenniDto getConfigurazioneMinorenniByIdServizio(Long idServizio) {
+
+        Optional<ConfigurazioneMinorenniEntity> confMinorenniOpt = configurazioneMinorenniRepository
+                .findByIdServizio(idServizio);
+
+        if (confMinorenniOpt.isEmpty()) {
+            // Restituisci un oggetto vuoto
+            return new ConfigurazioneMinorenniDto();
+        }
+
+        ConfigurazioneMinorenniEntity confMinorenni = confMinorenniOpt.get();
+        ConfigurazioneMinorenniDto confMinorenniDto = new ConfigurazioneMinorenniDto();
+        confMinorenniDto.setId(confMinorenni.getId());
+        confMinorenniDto.setIdProgramma(confMinorenni.getIdProgramma());
+        confMinorenniDto.setIntervento(confMinorenni.getIntervento());
+        confMinorenniDto.setDataAbilitazione(confMinorenni.getDataAbilitazione());
+        confMinorenniDto.setDataDecorrenza(confMinorenni.getDataDecorrenza());
+        confMinorenniDto.setCreateTimestamp(confMinorenni.getCreateTimestamp());
+        confMinorenniDto.setCreateUser(confMinorenni.getCreateUser());
+        confMinorenniDto.setUpdateTimestamp(confMinorenni.getUpdateTimestamp());
+        confMinorenniDto.setUpdateUser(confMinorenni.getUpdateUser());
+        return confMinorenniDto;
+    }
+}
