6f7ea71a796f0d572529d1b021ca73e48ae62273
lcucuzzella <lorenzo.cucuzzella@dxc.com>
2025-06-09 17:08:14 +0200

    feat(U345): aggiunta link assistenza footer 20250609

---
+++ fe-piattaforma/src/pages/administrator/Assistenza/assistenzaUtils.ts
@@ -0,0 +1,22 @@
+export const hrefValues = [
+    {
+        label: 'VademecumFacilitatori',
+        href: 'https://drive.repdigitale.org/lib/026b123e-08a4-414c-8f65-ff583cd9dd14/file/Documenti%20Facilita/Vademecum%20per%20enti%20gestori%20e%20partner%20di%20progetto%20per%20l%E2%80%99inserimento%20di%20enti%2C%20sedi%20e%20facilitatori.pdf',
+    },
+    {
+        label: 'ManualeFacilitazione',
+        href: 'https://www.notion.so/dtd-gov/Manuale-della-facilitazione-eaf5fec8773d462b9c76135d2dbb7bf9',
+    },
+    {
+        label: 'VademecumVolontari',
+        href: 'https://drive.repdigitale.org/f/dc93f6c59a20499aa835/',
+    },
+    {
+        label: 'ManualeVolontario',
+        href: 'https://www.notion.so/dtd-gov/Manuale-dell-Operatore-Volontario-fe358c74916642a3a383da04487d4746',
+    },
+    {
+        label: 'VademecumEntiGestoriPartner',
+        href: 'http://drive.repdigitale.org/f/36bd9d83064c4587844d/',
+    }
+]
