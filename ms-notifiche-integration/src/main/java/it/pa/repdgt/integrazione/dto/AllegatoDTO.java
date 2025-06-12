5b76f9322674a505a2eb0b8ca234d038ccc5157f
federico-tocci-dxc <federico.tocci@dxc.com>
2025-06-12 11:58:04 +0200

    feat(U346): aggiunta gestione allegati BE 20250612

---
+++ ms-notifiche-integration/src/main/java/it/pa/repdgt/integrazione/dto/AllegatoDTO.java
@@ -0,0 +1,12 @@
+package it.pa.repdgt.integrazione.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AllegatoDTO {
+    
+    private String name;
+    private String data;
+}
