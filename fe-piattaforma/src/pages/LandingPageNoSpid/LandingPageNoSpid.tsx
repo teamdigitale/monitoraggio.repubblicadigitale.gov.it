1106c6d68dd5f82b6ed05c3a6afeaf6999d2d2cf
lcucuzzella <lorenzo.cucuzzella@dxc.com>
2025-11-18 15:07:20 +0100

    feat(U465): implementazione pagina login NOSPID 20251118

---
+++ fe-piattaforma/src/pages/LandingPageNoSpid/LandingPageNoSpid.tsx
@@ -0,0 +1,39 @@
+import clsx from 'clsx';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import imgLaptop from '/public/assets/img/landing-page-img.jpg';
+import { Button } from 'design-react-kit';
+
+const LandingPageNoSpid: React.FC = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div className="my-5" style={{ display: 'flex', flexDirection: 'row', gap: '4rem', alignItems: 'center', justifyContent: 'space-between' }}>
+            {/* Colonna Testuale */}
+            <div>
+                <p
+                    className={clsx(
+                        'font-weight-bold primary-color h4 mb-4'
+                    )}>                    
+                    Ti diamo il benvenuto su Facilita
+                </p>
+                <p className="text-gray-700 text-base md:text-lg max-w-md mb-5">
+                    Facilita è il progetto del Dipartimento per la trasformazione digitale per gestire e monitorare i servizi di facilitazione digitale promossi dalla Rete dei servizi di facilitazione digitale e dal Servizio civile digitale.
+                </p>
+                <Button className='cta-button' onClick={() => navigate('/authNOSPID')} color='primary'>
+                    Accedi
+                </Button>
+            </div>
+            <div>
+                <img
+                    src={imgLaptop}
+                    alt="Laptop with Facilita interface"
+                    className={clsx('w-full', 'max-w-md')}
+                    style={{ maxHeight: '400px', objectFit: 'cover' }}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default LandingPageNoSpid;
