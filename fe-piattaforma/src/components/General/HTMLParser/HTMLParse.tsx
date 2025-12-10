871d866a3e107ba5559a35c0a0a0d19dac1f886c
Salvatore Gravina <salvatore.gravina@dxc.com>
2025-12-10 11:55:34 +0100

    fix(secu-wave2): remediation SAST 20251210

--- fe-piattaforma/src/components/General/HTMLParser/HTMLParse.tsx
+++ fe-piattaforma/src/components/General/HTMLParser/HTMLParse.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import DOMPurify from 'dompurify';
 
 interface HTMLParseI {
   html?: string | undefined;
 }
 
 const HTMLParser = ({ html = '<p></p>' }: HTMLParseI) => (
-  <div dangerouslySetInnerHTML={{ __html: html }} />
+  <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(html) }} />
 );
 
 export default HTMLParser;
